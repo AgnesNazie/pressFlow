c35d46de7b9165030ee42acff24942ab03c210b7
AgnesNazie <cheagnesmbo@gmail.com>
2026-01-06 18:30:50 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/pressflow/PressFlowApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.pressflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PressFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PressFlowApplication.class, args);
+    }
+
+}
